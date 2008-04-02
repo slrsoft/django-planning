f08735b90f80accc59dc4a56070f948f293df7d2
esimorre <esimorre@free.fr>
2008-04-02 21:57:05 +0000

    modeling (40mn)

--- planning/cal/models.py
+++ planning/cal/models.py
@@ -1,3 +1,42 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    manager = models.BooleanField(default=False)
+    class Admin:
+        pass
+
+class TypeEvent(models.Model):
+    file = models.ImageField(upload_to='images')
+    name = models.CharField(max_length=100, null=True, blank=True)
+    type = models.CharField(max_length=1, choices=(('1','congé'),), default='1')
+    
+    def graphic(self):
+        return '<img name=image%d src=%s>' % (self.id, self.get_file_url())
+    graphic.allow_tags=True
+    
+    class Admin:
+        list_display = ('name', 'graphic', 'type')
+    def __unicode__(self):
+        return self.name
+
+class DayEvent(models.Model):
+    user = models.ForeignKey(User)
+    valid_user = models.ForeignKey(User, null=True, blank=True, related_name='valid_dayevent_set')
+    type = models.ForeignKey(TypeEvent)
+    day = models.DateField()
+    class Admin:
+        list_display = ('user', 'day', 'type', 'valid_user')
+
+class Planning(models.Model):
+    user = models.ForeignKey(User, null=True, blank=True)
+    name = models.CharField(max_length=50, verbose_name='name')
+    desc = models.TextField(verbose_name='description')
+    code = models.CharField(max_length=50, verbose_name='code')
+    
+    def __unicode__(self):
+        return self.name
+    class Admin:
+        list_display = ('name','user')
