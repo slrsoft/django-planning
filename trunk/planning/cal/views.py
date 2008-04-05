0ab26f37b6cafb33d99d5775ecff7c0eb3cb7bc4
esimorre@free.fr <esimorre@free.fr@c001a3ed-e549-0410-a79e-2b208c66a47c>
2008-04-05 08:30:35 +0000

    daylist generation 30 mn

--- trunk/planning/cal/views.py
+++ trunk/planning/cal/views.py
@@ -4,16 +4,15 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse, HttpResponseRedirect
 from models import DayEvent, TypeEvent
 from django.contrib.auth.models import User
-from datetime import date, timedelta
+from datetime import date, datetime, timedelta
 
-type_vide,c = TypeEvent.objects.get_or_create()
+type_vide,c = TypeEvent.objects.get_or_create(name='empty', type='0')
 
 class day:
-    day_week
-    day_month
-    day_event
-    def __init__(self, date=None, day_event=None):
-         pass
+    def __init__(self, day=None, day_event=None):
+        if day:
+            self.month = day.month
+            self.year, self.week, self.day_week = day.isocalendar()
     
 
 def main(request, template='planning.html'):
@@ -26,7 +25,19 @@ def main(request, template='planning.html'):
     else:
         context['user_state'] = 'alt'
     context['user_id'] = request.session['user_id']
-    
+    today = datetime.now().date()
+    days = []
+    for d in range(10, 0, -1):
+        day_date = today - timedelta(days=d)
+        if day_date.year < today.year:
+            continue
+        days.append(day(day_date))
+    for d in range(0, 400):
+        day_date = today + timedelta(days=d)
+        if day_date.year > today.year:
+            break
+        days.append(day(day_date))
+    context['days'] = days
     
     
     return render_to_response(template, context)
