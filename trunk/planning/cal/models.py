702c5340c1df35bcdfbd71245369b1de832cb83e
esimorre@free.fr <esimorre@free.fr@c001a3ed-e549-0410-a79e-2b208c66a47c>
2008-04-07 21:10:41 +0000

    fix user profile gen code for user and planning test gen code 20 mn

--- trunk/planning/cal/models.py
+++ trunk/planning/cal/models.py
@@ -3,9 +3,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from random import choice
+import string
+def gen_string(length=32, chars=string.letters + string.digits):
+    return ''.join([choice(chars) for i in range(length)])
+
 class Profile(models.Model):
-    manager = models.BooleanField(default=False)
+    user = models.ForeignKey(User, verbose_name='utilisateur', unique=True, edit_inline=True, max_num_in_admin=1, num_in_admin=1)
+    manager = models.BooleanField(default=False, core=True)
     code = models.CharField(max_length=50, editable=False, unique=True, verbose_name='code')
+    
+    def save(self):
+        if not self.code:
+            self.code = gen_string()
+        models.Model.save(self)
+    
     class Admin:
         pass
 
@@ -37,6 +49,11 @@ class Planning(models.Model):
     name = models.CharField(max_length=50, verbose_name='name')
     desc = models.TextField(verbose_name='description')
     code = models.CharField(max_length=50, verbose_name='code')
+    
+    def save(self):
+        if not self.code:
+            self.code = gen_string()
+        models.Model.save(self)
     
     def __unicode__(self):
         return self.name
