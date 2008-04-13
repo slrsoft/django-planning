5e335f4ff229bb398e63a613f222957558fa84d5
esimorre <esimorre@free.fr>
2008-04-13 16:08:38 +0000

    planning edition 60 mn

--- planning/cal/views.py
+++ planning/cal/views.py
@@ -8,6 +8,7 @@ from datetime import date, timedelta
 from calendar import *
 
 from forms import PlanningSettingsForm, PlanningForm
+from models import Planning, PlanningSettings, TypeEvent
 
 type_vide,c = TypeEvent.objects.get_or_create(name='empty', type='0')
 
@@ -33,9 +34,34 @@ def add_planning(request, redirect='..'):
         form1 = PlanningForm(request.POST)
         form = PlanningSettingsForm(request.POST)
         if form.is_valid():
-            # Do form processing here...
+            p = form1.save(commit=False)
+            p.user = request.user
+            p.save()
+            pset = form.save(commit=False)
+            pset.planning = p
+            pset.save()
+            redirect = redirect % p.id
             return HttpResponseRedirect(redirect)
     else:
         form1 = PlanningForm()
         form = PlanningSettingsForm()
     return render_to_response('add_planning.html', {'form': form, 'form1': form1})
+
+
+def edit(request, id, template='edit.html', redirect='..'):
+    p = Planning.objects.get(id=int(id))
+    pset = PlanningSettings.objects.get(planning=p, name='default')
+    types = TypeEvent.objects.all()
+    if request.method == 'POST':
+        form1 = PlanningForm(request.POST, instance=p)
+        form = PlanningSettingsForm(request.POST, instance=pset)
+        if form.is_valid():
+            # Do form processing here...
+            return HttpResponseRedirect(redirect)
+    else:
+        form1 = PlanningForm(instance=p)
+        form = PlanningSettingsForm(instance=pset)
+    return render_to_response(template, {'form': form,
+                                         'form1': form1,
+                                         'planning_url':'http://urlxxx',
+                                         'types':types})
