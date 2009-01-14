b829e395df0e45862cb7ea65555fec75ea74bbcd
esimorre <esimorre@free.fr>
2009-01-14 22:51:55 +0000

    inspectdb open-bookings 0.6.4 beta (28/11/2007)

---
+++ django_booking/booking/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from models import *
+
+class ParamProfileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'display_order')
+admin.site.register(ParamProfile, ParamProfileAdmin)
+
+
+class DataUserAdmin(admin.ModelAdmin):
+    list_display = ('login', 'last_name', 'first_name', 'profile', 'locked', 'admin', 'email')
+admin.site.register(DataUser, DataUserAdmin)
+
+class ParamFamilyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'sort_order')
+    ordering = ('sort_order',)
+admin.site.register(ParamFamily, ParamFamilyAdmin)
+
+class DataAdmin(admin.ModelAdmin):
+    list_display = ('name', 'family', 'manager')
+admin.site.register(Data, DataAdmin)
+
+class DataBookingAdmin(admin.ModelAdmin):
+    list_display = ('data', 'start', 'end', 'user', 'validated')
+admin.site.register(DataBooking, DataBookingAdmin)
+
+class ParamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'value')
+admin.site.register(Param, ParamAdmin)
+
+class ParamLangAdmin(admin.ModelAdmin):
+    list_display = ('english', 'french', 'german', 'norwegian')
+admin.site.register(ParamLang, ParamLangAdmin)
---
+++ django_booking/booking/models.py
@@ -0,0 +1,99 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class ParamProfile(models.Model):
+    profile_id = models.AutoField(primary_key=True)
+    display_order = models.IntegerField()
+    name = models.CharField(max_length=300, unique=True, db_column='profile_name')
+    
+    def __unicode__(self):
+        return self.name
+    class Meta:
+        db_table = u'rs_param_profiles'
+        verbose_name = 'Profile'
+
+class DataUser(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    first_name = models.CharField(max_length=150, blank=True)
+    login = models.CharField(unique=True, max_length=60, blank=True)
+    profile = models.ForeignKey(ParamProfile, db_column='profile_id')
+    email = models.CharField(max_length=240, blank=True)
+    password = models.CharField(max_length=60, blank=True)
+    locked = models.BooleanField()
+    admin = models.BooleanField()
+    remarks = models.CharField(max_length=765, blank=True)
+    
+    def __unicode__(self):
+        return self.login
+    class Meta:
+        db_table = u'rs_data_users'
+        verbose_name = 'openbookings user'
+
+class ParamFamily(models.Model):
+    family_id = models.AutoField(primary_key=True)
+    sort_order = models.IntegerField()
+    name = models.CharField(max_length=150, db_column='family_name')
+    
+    def __unicode__(self):
+        return self.name
+    class Meta:
+        db_table = u'rs_param_families'
+        verbose_name = 'Family'
+        verbose_name_plural = 'Families'
+
+class Data(models.Model):
+    object_id = models.AutoField(primary_key=True)
+    rand_code = models.IntegerField(null=True, blank=True)
+    name = models.CharField(max_length=150, db_column='object_name')
+    family = models.ForeignKey(ParamFamily)
+    manager = models.ForeignKey(DataUser)
+    misc_info = models.CharField(max_length=765, blank=True)
+    
+    def __unicode__(self):
+        return self.name
+    class Meta:
+        db_table = u'rs_data_objects'
+        verbose_name = 'Bookable object'
+
+class DataBooking(models.Model):
+    book_id = models.AutoField(primary_key=True)
+    rand_code = models.IntegerField(null=True, blank=True)
+    date = models.DateTimeField(db_column='book_date')
+    data = models.ForeignKey(Data, db_column='object_id')
+    user = models.ForeignKey(DataUser)
+    start = models.DateTimeField(db_column='book_start')
+    end = models.DateTimeField(db_column='book_end')
+    validated = models.BooleanField()
+    misc_info = models.CharField(max_length=765)
+    
+    def __unicode__(self):
+        return self.data.name
+    class Meta:
+        db_table = u'rs_data_bookings'
+        verbose_name = 'Booking'
+
+class Param(models.Model):
+    name = models.CharField(max_length=75, primary_key=True, db_column='param_name')
+    value = models.CharField(max_length=75, db_column='param_value')
+    
+    def __unicode__(self):
+        return self.name
+    class Meta:
+        db_table = u'rs_param'
+        verbose_name = 'Parameter'
+
+class ParamLang(models.Model):
+    lang_id = models.AutoField(primary_key=True)
+    english = models.CharField(max_length=765, blank=True)
+    french = models.CharField(max_length=765, blank=True)
+    german = models.CharField(max_length=765, blank=True)
+    norwegian = models.CharField(max_length=765, blank=True)
+    
+    def __unicode__(self):
+        return self.english
+    class Meta:
+        db_table = u'rs_param_lang'
+        verbose_name = 'Language parameter'
+
